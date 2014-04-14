d023d26ff86cb565584635800d7efb98f17440cb
Rodrigo Girão <rodrigogirao7@gmail.com>
2014-04-14 09:00:35 -0300

    Setting device specifications in activity

---
+++ ICA-Client/src/br/com/rolesoft/ica_client/model/DeviceSpecifications.java
@@ -0,0 +1,34 @@
+package br.com.rolesoft.ica_client.model;
+
+public class DeviceSpecifications {
+	int width;
+	int height;
+	double screenSize;
+	long avaiableMemory;
+	
+	
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public double getScreenSize() {
+		return screenSize;
+	}
+	public void setScreenSize(double screenSize) {
+		this.screenSize = screenSize;
+	}
+	public long getAvaiableMemory() {
+		return avaiableMemory;
+	}
+	public void setAvaiableMemory(long avaiableMemory) {
+		this.avaiableMemory = avaiableMemory;
+	}
+}
