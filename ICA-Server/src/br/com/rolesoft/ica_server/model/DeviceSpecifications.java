46cede37ff03ddd71d075fc0c04d7892d113fc0b
rodrigogirao <rodrigogirao7@gmail.com>
2014-05-19 19:52:18 -0300

    Change DeviceSpecification to match with Client

--- ICA-Server/src/br/com/rolesoft/ica_server/model/DeviceSpecifications.java
+++ ICA-Server/src/br/com/rolesoft/ica_server/model/DeviceSpecifications.java
@@ -5,30 +5,49 @@ public class DeviceSpecifications {
 	int height;
 	double screenSize;
 	long avaiableMemory;
-	
-	
+	private ConnectionType connectionType;
+
+	public enum ConnectionType {
+		WIFI, DATA_CONNECTION, NO_CONNECTION
+	}
+
 	public int getWidth() {
 		return width;
 	}
+
 	public void setWidth(int width) {
 		this.width = width;
 	}
+
 	public int getHeight() {
 		return height;
 	}
+
 	public void setHeight(int height) {
 		this.height = height;
 	}
+
 	public double getScreenSize() {
 		return screenSize;
 	}
+
 	public void setScreenSize(double screenSize) {
 		this.screenSize = screenSize;
 	}
+
 	public long getAvaiableMemory() {
 		return avaiableMemory;
 	}
+
 	public void setAvaiableMemory(long avaiableMemory) {
 		this.avaiableMemory = avaiableMemory;
+	}
+
+	public ConnectionType getConnectionType() {
+		return connectionType;
+	}
+
+	public void setConnectionType(ConnectionType connectionType) {
+		this.connectionType = connectionType;
 	}
 }
